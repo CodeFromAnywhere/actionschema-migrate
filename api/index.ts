626504672eac660fb88c55cb96df58abe0a991d3
Wijnand Karsens <wijnand@karsens.com>
2024-06-28 11:06:03 +0200

    Improvements

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,7 @@
 import fs from "node:fs";
-
+import path from "node:path";
 export const GET = () => {
-  const readmeContent = fs.readFileSync("README.md");
+  const readmeContent = fs.readFileSync(path.join(process.cwd(), "README.md"));
 
   return new Response(readmeContent, {
     status: 200,

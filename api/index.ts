410a98eea1a47c0492d85085b0ad9ed33323304f
Wijnand Karsens <wijnand@karsens.com>
2024-06-28 10:51:06 +0200

    Improvementst

--- api/index.ts
+++ api/index.ts
@@ -1,1 +1,5 @@
-export const GET = () => new Response("Hello world");
+export const GET = () =>
+  new Response("Hello world", {
+    status: 302,
+    headers: { Location: "/migration-context.schema.json" },
+  });
